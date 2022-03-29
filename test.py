92ee6473a1e6dfaef2e1b1b84dd51c945484a1ad
sssharma9268 <sssharma9268@gmail.com>
2022-03-29 19:32:15 +0530

    python file

---
+++ test.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+import os
+app = Flask(__name__)
+
+
+@app.route("/")
+
+def hello():
+    html = "<h3>Test:{test}</h3>"
+    test = os.environ['JAVA_HOME']
+
+    return html.format(test = test)
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=81)
